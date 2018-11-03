20b6c21802d9c21b182b25c55da48386f365a0f2
Bertoncini <alex.bertoncini@terra.com.br>
2018-11-03 07:09:56 -0300

    adicionado  factory para guardar status de usuario logado

--- LibWebSystem/App/App.js
+++ LibWebSystem/App/App.js
@@ -48,4 +48,18 @@
                 }
             });
         }])
+        .factory('Auth', function () {
+            var user;
+
+            return {
+                setUser: function (aUser) {
+                    user = aUser;
+                },
+                isLoggedIn: function () {
+                    return (user) ? user : false;
+                }
+            };
+        });
+
+
 }());
