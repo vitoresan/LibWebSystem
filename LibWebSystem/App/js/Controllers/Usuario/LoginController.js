e7fbc1f458b73932ee8f7649aee9b4c1eb3e0b13
Bertoncini <alex.bertoncini@terra.com.br>
2018-11-03 07:31:40 -0300

    adicionado metodo para realizar login

--- LibWebSystem/App/js/Controllers/Usuario/LoginController.js
+++ LibWebSystem/App/js/Controllers/Usuario/LoginController.js
@@ -2,9 +2,26 @@
 
     var app = angular.module('LibWebSystem');
 
-    var LoginController = function ($scope, $http, $log, $window, UsuarioServices, tabelaPadrao, toastr) {
+    var LoginController = function ($scope, $http, $log, $window, Auth, LoginService, toastr) {
 
+        $scope.login = function (usuario, senha) {
+            LoginService.Login(usuario, senha)
+                .then(onCompleteLogin, onError);
+        };
 
+        function onCompleteLogin(success) {
+            if (success.data.Status === 0) {
+                toastr.warning(success.data.Mensagem);
+            }
+            else {
+                Auth.setUser(success.data); 
+                $window.location.href = '/Index';
+            }
+        }
+
+        function onError(error) {
+            toastr.error('Erro ao realizar login: ' + error.data.Message);
+        }
 
         app.controller('LoginController', LoginController);
 
