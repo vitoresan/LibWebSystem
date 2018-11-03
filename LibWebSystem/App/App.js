5a0ab07f4394da1a8562f920aa7e9c01ec5b7d9c
Bertoncini <alex.bertoncini@terra.com.br>
2018-11-03 07:17:52 -0300

    adiciona intercepção de usuario não logado e redicionando para a tela de login

--- LibWebSystem/App/App.js
+++ LibWebSystem/App/App.js
@@ -61,5 +61,17 @@
             };
         });
 
+    app.run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
+        $rootScope.$on('$routeChangeStart', function (event) {
 
+            if (!Auth.isLoggedIn()) {
+                console.log('DENY');
+                $location.path('/Login');
+            }
+            else {
+                console.log('ALLOW');
+                $location.path('/Index');
+            }
+        });
+    }]);
 }());
