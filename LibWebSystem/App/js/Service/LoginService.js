06f2a1adeecf1f1886d74eb0c98a36c2136bb2eb
Bertoncini <alex.bertoncini@terra.com.br>
2018-11-03 07:29:57 -0300

    adicionado classe de serviço para acessar a api do token

---
+++ LibWebSystem/App/js/Service/LoginService.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var app = angular.module("LibWebSystem");
+
+app.factory('LoginService', ['$http', function ($http) {
+
+    var LoginService = {};
+
+    LoginService.Login = function (usuario, senha) {
+        return $http.post(app.WebApi + "Token", "userName=" + encodeURIComponent(usuario) +
+            "&password=" + encodeURIComponent(senha) +
+            "&grant_type=password"
+    };
+
+    return LoginService;
+
+}]);
